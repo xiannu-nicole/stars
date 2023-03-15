73c9242c707896400783cac10b60d2b384f52b3f
xiannu-nicole <nicole16312@gmail.com>
2023-03-15 10:04:48 +0800

    <viewport>

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/stars/'
-//     : './'
-// }
+
 module.exports = {
   publicPath: './',
     outputDir: 'dist',
